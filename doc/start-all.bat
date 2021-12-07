605877ff424727a61b3df0e6ac18766ba7f6ca62
anxiaolong <anxiaolong@mypc>
2021-12-07 11:10:32 +0800

    update

---
+++ doc/start-all.bat
@@ -0,0 +1,4 @@
+start cmd /k java -jar internet-dubbo-provider-1.0-SNAPSHOT.jar
+start cmd /k java -jar internet-order-rest-1.0-SNAPSHOT.jar
+start cmd /k java -jar quick-order-management-1.0-SNAPSHOT.jar
+start cmd /k java -jar quick-order-supplier-1.0-SNAPSHOT.jar
